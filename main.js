631d5344371f73c555320bf2ad3c6fffb86618e7
Mads Lentoft <s183919@student.dtu.dk>
2025-09-22 22:29:51 +0200

    Change to letters

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let letters = ['W','E','F','A','M','g','u','t','t','m','s','a','t','o','d','a','t','o','p','w','t','A','k','b','b','t','W','T','E','K','F','N','a','A','N','B','t','a','c','w','t','F','N','a','O','t','A','m','a','f','e','O','h','c','s','t','r','f','B','w','t','w','n','h','m','h','v','A','h','y','h','p','a','t','F','N','i','n','v','i','t','W','T','y','a','m','f','a','t','m','o','m','t','j','t','t','E','K','t','h','f','a','t','F','N','l','m','a','m','b','t','l','a','o','t','S','p','b','t','t','A','w','n','r','i','t','A','N','a','t','t','c','i','b','B','I','h','l','h','I','s','b','t','s','t','A','w','r','t','s','t','w'];
+let letters = ['<span>W</span>.','<span>E</span>.','<span>F</span>.','<span>A</span>.','L','a,','t','f','n','l','t','i','h.','T,','e','c','w','t','F','N','a.','O','t','A,','m','o','a','f','e,','c','s','t,','b','w','t','w','n','h','m,','h','v.','A','h','y','p','a','m','b','a','I','d','t','n','A,','a','a','n','A.','A','a','h','a','s','a','g,','h','h','a','l','t','l','b','h','r','t','s','a.','B','I','b','A','c','s','t','w.'];
 
 function addDays(date, days) {
     var result = new Date(date);
@@ -7,7 +7,7 @@ function addDays(date, days) {
 }
 
 window.onload = function() {
-    let offset = 4;
+    let offset = 2;
 
     let oneDay = 24 * 60 * 60 * 1000;
     let today = new Date()
@@ -18,9 +18,9 @@ window.onload = function() {
     let diffDays = Math.round(Math.abs((startdate - enddate) / oneDay));
     let progDays = Math.round(Math.abs((startdate - today) / oneDay));
 
-    let text = letters.slice(0, letters.length * progDays / diffDays + offset);
+    //let text = letters.slice(0, letters.length * progDays / diffDays + offset);
+    let text = letters.slice(0, 2 * progDays + offset);
 
     document.body.style = "font-size:" + (text.length > 100 ? 32 : Math.min(3000 / text.length, 80)) + "px";
     document.body.innerHTML = text.join(" ");
 }
-
